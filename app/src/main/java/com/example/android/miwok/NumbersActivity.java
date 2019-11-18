6babf0e85977153c2b8e7505b12eb05d70e0d090
Robert Beckman <rbeckman@pacificbiosciences.com>
2019-11-18 15:25:02 -0800

    Delete some obsolete code.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,7 +1,7 @@
 package com.example.android.miwok;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+        import androidx.appcompat.app.AppCompatActivity;
+        import android.os.Bundle;
 
 public class NumbersActivity extends AppCompatActivity {
 
